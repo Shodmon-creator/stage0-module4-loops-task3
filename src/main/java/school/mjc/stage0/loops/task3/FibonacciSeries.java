14c620c020fe237ce4110a64854b421c0086f265
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2022-11-03 22:41:00 +0500

    6 tasks have been done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int numberOne=0;
+        int numberTwo=1;
+        int fibonacci = 0;
+        for (int i = 1; i<lastFibonacci; i++) {
+            if (fibonacci==1)
+                System.out.println(numberOne);
+            System.out.println(fibonacci);
+            fibonacci=numberOne+numberTwo;
+            numberOne = numberTwo;
+            numberTwo=fibonacci;
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int i = 0;
+
+
+        if (first == 0 || second == 0) {
+            System.out.println(Math.max(first, second));
+        } else if (first == second) {
+            System.out.println(first);
+        } else if (first > second) {
+            i = second;
+        } else {
+            i = first;
+        }
+        for (i = i; i > 1; i--) {
+            if (first % i == 0 && second % i == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
